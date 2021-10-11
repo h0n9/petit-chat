54f8c12a434971094398e4083d5ea1b5d8c62f60
Jaehong Ahn <monsieur.ahn@gmail.com>
2021-10-11 17:00:38 +0900

    Add test codes for marshaling

---
+++ crypto/key_test.go
@@ -0,0 +1,36 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrivKeyMarshal(t *testing.T) {
+	privKey, err := GenPrivKey()
+	assert.Nil(t, err)
+
+	b, err := privKey.MarshalJSON()
+	assert.Nil(t, err)
+
+	newPrivKey := PrivKey{}
+	err = newPrivKey.UnmarshalJSON(b)
+	assert.Nil(t, err)
+
+	assert.Equal(t, privKey, newPrivKey)
+}
+func TestPubKeyMarshal(t *testing.T) {
+	privKey, err := GenPrivKey()
+	assert.Nil(t, err)
+
+	pubKey := privKey.PubKey()
+
+	b, err := pubKey.MarshalJSON()
+	assert.Nil(t, err)
+
+	newPubKey := PubKey{}
+	err = newPubKey.UnmarshalJSON(b)
+	assert.Nil(t, err)
+
+	assert.Equal(t, pubKey, newPubKey)
+}
